d45f1b120b858f84b8b68ca063de4b2b8b8d6c07
Zakhar <zakhar6701067@gmail.com>
2024-04-26 10:31:13 +0300

    upgrade

--- js/playImg.js
+++ js/playImg.js
@@ -3,33 +3,36 @@ var btn = document.getElementById("heartTxt");
 btn.style.opacity = 0;
 var btnVal = 0;
 
-function showImage(){
-	//document.getElementById("imgTxt").style.opacity = 0;
-	myImage.setAttribute("src", imageArray[imageIndex]);
-	myTxt.innerHTML = txtArray[imageIndex];
-	//document.getElementById("imgTxt").style.opacity = 1 - flag;
-	imageIndex++;
-	if(imageIndex >= len){
-		imageIndex = 0;
-	}
+function showImage() {
+    if (imageIndex < len) {
+        myImage.setAttribute("src", imageArray[imageIndex]);
+        myTxt.innerHTML = txtArray[imageIndex];
+        imageIndex++;
+    } else {
+        clearInterval(showImageInterval);
+    }
 }
 
-function play(){
-	if(t == 0){
-		myImage.setAttribute("src", "");
-		myTxt.innerHTML = "";
-		imageIndex = 0;
-		clearInterval(showImageInterval);
-	}
-	flag = 1 - flag;
-	document.getElementById("typeDiv").style.opacity = flag;
-	document.getElementById("imgTxt").style.opacity = 1 - flag;
-	if(t == 0){
-		//setTimeout(showImage, 1000);
-		setInterval(showImage, 2500);
-	}
-	t++;
+
+function play() {
+    if (t == 0) {
+        myImage.setAttribute("src", "");
+        myTxt.innerHTML = "";
+        imageIndex = 0;
+        clearInterval(showImageInterval);
+        showImageInterval = setInterval(showImage, 2500);  // Перезапускаем показ изображений
+    }
+    flag = 1 - flag;
+    document.getElementById("typeDiv").style.opacity = flag;
+    document.getElementById("imgTxt").style.opacity = 1 - flag;
+
+    if (t == 0) {
+        t++;
+    } else {
+        t = 0;  // Сбрасываем t для возможности повторного запуска
+    }
 }
+
 
 function preshowImage(){
 	document.getElementById("imgTxt").style.opacity = 0;
